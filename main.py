49414c747ebd8d9d42ef053e48756ec09b719fb1
Alex Li <cli5@snapchat.com>
2022-05-16 10:30:14 -0400

    wait for each item

--- main.py
+++ main.py
@@ -17,11 +17,11 @@ hand_bag2 = {
 
 items_to_buy = [unavailable_black_totle, available_hand_bag, hand_bag2]
 
-
 try:
     sales_person = Sales()
     sales_person.add_items(items_to_buy)
-    sales_person.set_max_duration_hour(0.5)
+    sales_person.set_wait_time_second(3)
+    sales_person.set_max_duration_hour(0.02)
     sales_person.run()
 except RuntimeError as e:
     print(e)
--- shop/sales.py
+++ shop/sales.py
@@ -1,4 +1,3 @@
-from email import message
 from time import sleep, time
 import requests
 import webbrowser
@@ -16,26 +15,24 @@ class Sales:
             item['status'] = 'unavailable'
             self.items.append(item)    
 
-    def set_wait_time(self, wait_time):
+    def set_wait_time_second(self, wait_time):
         self.wait_time = wait_time
     
     def set_max_duration_hour(self, duration):
         self.max_duration_hour = duration
-        print(self.max_duration_hour)
 
     def run(self):
         start_time = time()
         while True:            
             if time() - start_time > self.max_duration_hour * 3600:
+                print('Reached max duration.')
                 break
 
             if self.all_items_available():
+                print('All items are available.')
                 break
-            else:
-                self.udpate_items()
-            
-            sleep(self.wait_time)
-
+        
+            self.udpate_items()
         
     def all_items_available(self):
         for item in self.items:
@@ -45,11 +42,13 @@ class Sales:
 
     def udpate_items(self):
         for i in range(0, len(self.items)):
-            status = self.get_item_status(self.items[i])
+            new_status = self.get_item_status(self.items[i])
             
-            if status != self.items[i]['status']:                
-                self.items[i]['status'] = status                
+            if new_status != self.items[i]['status']:                
+                self.items[i]['status'] = new_status
                 self.notify_customer_about_item(self.items[i])
+            
+            sleep(self.wait_time)
             
     def prepare_headers(self) -> object:
         headers_dict = self.parse_header_file('shop/header')
@@ -62,26 +61,40 @@ class Sales:
         return headers
 
     def parse_header_file(self, file_path):
-        headers = dict()
+        headers_dict = dict()
         f = open(file_path)
         for line in f:
             i = line.find(':')
             name = line[:i]
             val = line[i+1:]
-            headers[name] = val.strip()
+            headers_dict[name] = val.strip()
         f.close()
-        return headers
+        return headers_dict
 
     def get_item_status(self, item):
+        if item['status'] == 'available':
+            return 'available'
+
         res = requests.get(item['url'], headers=self.headers)
+        
         if res.status_code == 403:
-            raise RuntimeWarning("Unauthorized to access the website")
+            message = 'Unauthorized access to website.\n'
+            message += '=================\n'
+            message += 'Item name: ' + item['name'] + '\n' 
+            message += 'Item url: ' + item['url'] + '\n' 
+            message += 'Cause: header file outdated or IP being blocked .\n'
+            message += '=================\n'
+
+            raise RuntimeWarning(message)
+        
         elif res.status_code == 404:            
             print('Item unavailable')
             return 'unavailable'
+        
         elif res.status_code == 200:
             print('Item available')
             return 'available'
+        
         else:
             print('Item might be available', '\nstatus code: ', res.status_code)            
             return 'unknown'
